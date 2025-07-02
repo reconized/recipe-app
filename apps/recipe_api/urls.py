85d2ada43e32c2199b533882e3fdc1c002d1bb68
r3dz3d <it602msu@gmail.com>
2025-07-02 17:58:08 -0500

    Define endpoints

--- apps/recipe_api/urls.py
+++ apps/recipe_api/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from apps.recipe_api import views
 
 urlpatterns = [
-    
+    # Categories endpoints
+    # path('categories/', views.CategoriesView.as_view()),
+    # path('categories/<int:pk>/', views.CategoryDetailView.as_view()),
+
+    # Recipes endpoints
+    # path('recipes/', views.RecipeListCreateView.as_view()),
+    # path('recipes/<int:pk>/', views.RecipeDetailView.as_view()),
+
+    # Manager group management endpoints
+    # path('groups/manager/users/', views.ManagerGroupUserListCreateView.as_view()),
+    # path('groups/manager/users/<int:pk>/', views.ManagerGroupUserDeleteView.as_view()),
 ]
