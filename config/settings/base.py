cd5c88d923b341b0f7c253aedd9b8b7497c9f9f4
r3dz3d <it602msu@gmail.com>
2025-07-12 14:45:00 -0500

    Fix custom password validator errors

--- config/settings/base.py
+++ config/settings/base.py
@@ -173,7 +173,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 4,
     'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.ScopedThrottle',
+        'rest_framework.throttling.ScopedRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
         'users': '1000/day',
--- config/validators.py
+++ config/validators.py
@@ -1,7 +1,7 @@
 import re
 
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 class MinimumLengthValidator:
     def __init__(self, min_length=8):
@@ -22,8 +22,12 @@ class MinimumLengthValidator:
         )
     
 class NumberValidator(object):
+    def __init__(self, min_digits=2):
+        self.min_digits = min_digits
+
+
     def validate(self, password, user=None):
-        if not re.findall('\d', password):
+        if not re.findall(r'\d', password):
             raise ValidationError(
                 _("The password must contain at least 1 digit, 0-9."),
                 code='password_no_number',
@@ -62,15 +66,15 @@ class LowerCaseValidator(object):
     
 class SymbolValidator(object):
     def validate(self, password, user=None):
-        if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
+        if not re.findall(r'[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
             raise ValidationError(
                 _("The password must contain at least 1 symbol: " + 
-                  "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"),
+                  r"()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"),
                   code='password_no_symbol',
             )
         
     def get_help_text(self):
         return _(
             "Your password must contain at least 1 symbol: " + 
-            "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"
+            r"()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"
         )
