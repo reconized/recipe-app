2238d4dc5016143de165f530c7d698e624d80dbd
r3dz3d <it602msu@gmail.com>
2025-08-11 22:57:40 -0500

    deploy

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-curl -LsSf https://astral.sh/uv/install.sh | env UV_UNMANAGED_INSTALL="/tmp/.uv" sh
-export PATH="/tmp/.uv/bin:$PATH"
-uv sync
-uv run python manage.py collectstatic --noinput
-uv run python manage.py migrate --noinput && uv run python manage.py collectstatic --noinput
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
